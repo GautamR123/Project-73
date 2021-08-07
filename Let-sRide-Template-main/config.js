368bc2a36c317d7f51c726f36916d94709634d09
GautamR123 <75315987+GautamR123@users.noreply.github.com>
2021-08-07 14:07:26 +0530

    Add files via upload

---
+++ Let-sRide-Template-main/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+require("@firebase/firestore")
+
+
+ var firebaseConfig = {
+  apiKey: "AIzaSyAm2mk4Bgj1a3Dol1xcFAKinLPrLhDgcjg",
+  authDomain: "project-73-40a85.firebaseapp.com",
+  projectId: "project-73-40a85",
+  storageBucket: "project-73-40a85.appspot.com",
+  messagingSenderId: "667829994448",
+  appId: "1:667829994448:web:c7cd0c399e18b720276fd5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore()
+
